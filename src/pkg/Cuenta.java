6d025235b102c84ae92303d2733f5d674e744544
rocioph <rocioph70@gmail.com>
2023-10-16 12:00:48 +0200

    version 4 paso 2 TDD constructor Cuenta añadido el parametro numero de cuenta

--- src/pkg/Cuenta.java
+++ src/pkg/Cuenta.java
@@ -4,10 +4,12 @@ public class Cuenta {
 
 	private double saldo;
 	private static final double saldomin=-500;
+	private double numero;
 	
-	public Cuenta(double saldo) {
+	public Cuenta(double saldo, int numero) {
 		super();
 		this.saldo = saldo;
+		this.numero=12345;
 	}
 	public double getSaldo() {
 		return saldo;
